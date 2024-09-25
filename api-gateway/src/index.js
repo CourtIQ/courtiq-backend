0467b2195912c1d8c815a0c07cc294987c77f151
Pranav Suri <pranavsuri@icloud.com>
2024-09-24 20:26:14 -0400

    fix some shit

--- api-gateway/src/index.js
+++ api-gateway/src/index.js
@@ -1,24 +1,17 @@
+const { ApolloGateway } = require('@apollo/gateway');
 const { ApolloServer } = require('apollo-server');
-const { ApolloGateway, IntrospectAndCompose } = require('@apollo/gateway');
 
 const gateway = new ApolloGateway({
-  supergraphSdl: new IntrospectAndCompose({
-    subgraphs: [
-      { name: 'user', url: process.env.USER_SERVICE_URL },
-      { name: 'string', url: process.env.STRING_SERVICE_URL },
-      { name: 'matchup', url: process.env.MATCHUP_SERVICE_URL },
-      { name: 'relationship', url: process.env.RELATIONSHIP_SERVICE_URL },
-    ],
-  }),
+  serviceList: [
+    { name: 'user', url: 'http://user-service:8081/query' },
+    { name: 'string', url: 'http://string-service:8082/query' },
+    { name: 'relationship', url: 'http://relationship-service:8083/query' },
+    { name: 'matchup', url: 'http://matchup-service:8084/query' },
+  ],
 });
 
-const server = new ApolloServer({
-  gateway,
-  subscriptions: false,
-});
+const server = new ApolloServer({ gateway });
 
-server.listen({ port: 4000 }).then(({ url }) => {
+server.listen().then(({ url }) => {
   console.log(`🚀 Gateway ready at ${url}`);
-}).catch(err => {
-  console.error('Failed to start the Apollo Gateway:', err);
 });
